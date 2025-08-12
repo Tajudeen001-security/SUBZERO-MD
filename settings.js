662fe336c6f74fa625d84d1d976b0bc33b93bb1f
Tajudeen001-security <gbadamositajudeenwan@gmail.com>
2025-08-12 06:34:07 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
   SESSION_ID: "", // Put Session Id Here Inside "SUBZERO-MD;;;WVNBQbID#p2LcPMqHMjfW5bQClqDjK0KufM3F_QcwSYRwO8PnYW0"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  OWNER_NUMBER: "2349160654415", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
